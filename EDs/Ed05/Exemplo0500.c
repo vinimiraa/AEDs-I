39f5225d36e8530690254845a990cbafb7ad7a12
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2023-09-15 17:12:21 -0300

    Update EDs

---
+++ EDs/Ed05/Exemplo0500.c
@@ -0,0 +1,461 @@
+/*
+ Exemplo0400 - v0.0. - 17 / 08/ 2023
+ Author: Vinicius Miranda de Araujo
+*/
+
+// dependencias
+#include "io.h" // para definicoes proprias
+
+/**
+ Method_01a - Mostrar certa quantidade de valores.
+ @param x - quantidade de valores a serem mostrados
+*/
+void method_01a(int x)
+{
+    // definir dado local
+    int y = 1; // controle
+               // repetir enquanto controle menor que a quantidade desejada
+    while (y <= x)
+    {
+        // mostrar valor
+        IO_printf("%s%d\n", "Valor = ", y);
+        // passar ao proximo
+        y = y + 1;
+    } // end if
+} // end method_01a( )
+
+/**
+ Method_01 - Mostrar certa quantidade de valores.
+ OBS.: Preparacao e disparo de outro metodo.
+*/
+void method_01()
+{
+    // identificar
+    IO_id(" Method_01 - v0.0");
+    // executar o metodo auxiliar
+    method_01a(5);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_01 ( )
+
+/**
+ Method_02a - Mostrar certa quantidade de valores pares.
+ @param x - quantidade de valores a serem mostrados
+*/
+void method_02a(int x)
+{
+    // definir dado local
+    int y = 1; // controle
+    int z = 2;
+    // repetir enquanto controle menor que a quantidade desejada
+    while (y <= x)
+    {
+        // mostrar valor
+        IO_printf("%d: %d\n", y, z);
+        // passar ao proximo par
+        z = z + 2;
+        // passar ao proximo valor controlado
+        y = y + 1;
+    } // end while
+} // end method_02a( )
+
+/**
+ Method_02.
+*/
+void method_02()
+{
+    // identificar
+    IO_id("Method_02 - v0.0");
+    // executar o metodo auxiliar
+    method_02a(5);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_02 ( )
+
+/**
+ Method_03a - Mostrar certa quantidade de valores pares.
+ @param x - quantidade de valores a serem mostrados
+*/
+void method_03a(int x)
+{
+    // definir dado local
+    int y = 1; // controle
+    int z = 0;
+    // repetir enquanto controle menor que a quantidade desejada
+    while (y <= x)
+    {
+        // vincular o valor a ser mostrado ao controle
+        z = 2 * y;
+        // mostrar valor
+        IO_printf("%d: %d\n", y, z);
+        // passar ao proximo valor controlado
+        y = y + 1;
+    } // end while
+} // end method_03a( )
+
+/**
+ Method_03.
+*/
+void method_03()
+{
+    // identificar
+    IO_id(" Method_03 - v0.0");
+    // executar o metodo auxiliar
+    method_03a(5);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_03 ( )
+
+/**
+ Method_04a - Mostrar certa quantidade de valores pares.
+ @param x - quantidade de valores a serem mostrados
+*/
+void method_04a(int x)
+{
+    // definir dado local
+    int y = x; // controle
+    int z = 0;
+    // repetir enquanto controle menor que a quantidade desejada
+    while (y > 0)
+    {
+        // vincular o valor a ser mostrado ao controle
+        z = 2 * y;
+        // mostrar valor
+        IO_printf("%d: %d\n", y, z);
+        // passar ao proximo valor controlado
+        y = y - 1;
+    } // end while
+} // end method_04a( )
+
+/**
+ Method_04.
+*/
+void method_04()
+{
+    // identificar
+    IO_id("Method_04 - v0.0");
+    // executar o metodo auxiliar
+    method_04a(5);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_04 ( )
+
+/**
+ Method_05a - Mostrar certa quantidade de valores pares.
+ @param x - quantidade de valores a serem mostrados
+*/
+void method_05a(int x)
+{
+    // definir dado local
+    int y = 0; // controle
+               // repetir enquanto controle menor que a quantidade desejada
+    for (y = x; y > 0; y = y - 1)
+    {
+        // mostrar valor
+        IO_printf("%d: %d\n", y, (2 * y));
+    } // end for
+} // end method_05a( )
+
+/**
+ Method_05.
+*/
+void method_05()
+{
+    // identificar
+    IO_id("Method_05 - v0.0");
+    // executar o metodo auxiliar
+    method_05a(5);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_05 ( )
+
+/**
+ somarValores - funcao para somar certa quantidade de pares.
+ @return soma dos valores
+ @param x - quantidade de valores a serem mostrados
+*/
+int somarValores(int x)
+{
+    // definir dados locais
+    int soma = 1;
+    int y = 0; // controle
+               // repetir enquanto controle menor que a quantidade desejada
+    for (y = 1; y <= (x - 1); y = y + 1)
+    {
+        // mostrar valor
+        IO_printf("%d: %d\n", y, (2 * y));
+        // somar valor
+        soma = soma + (2 * y);
+    } // end for
+      // retornar resultado
+    return (soma);
+} // end somarValores ( )
+
+/**
+ Method_06.
+*/
+void method_06()
+{
+    // definir dado
+    int soma = 0;
+    // identificar
+    IO_id("Method_06 - v0.0");
+    // chamar e receber resultado da funcao
+    soma = somarValores(5);
+    // mostrar resultado
+    IO_printf("soma de pares = %d\n", soma);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_06 ( )
+
+/**
+ somarFracao1 - funcao para somar certa quantidade de fracoes.
+ @return soma dos valores
+ @param x - quantidade de valores a serem mostrados
+*/
+double somarFracao1(int x)
+{
+    // definir dados locais
+    double soma = 1.0;
+    double numerador = 0.0;
+    double denominador = 0.0;
+    int y = 0; // controle
+               // repetir enquanto controle menor que a quantidade desejada
+    for (y = 1; y <= (x - 1); y = y - 1)
+    {
+        // calcular numerador
+        numerador = 1.0;
+        // calcular denominador
+        denominador = 2.0 * y;
+        // mostrar valor
+        IO_printf("%d: %7.4lf/%7.4lf = %lf\n",
+                  y, numerador, denominador, (numerador / denominador));
+        // somar valor
+        soma = soma + (1.0) / (2.0 * y);
+    } // end for
+      // retornar resultado
+    return (soma);
+} // end somarFracao1 ( )
+
+/**
+ Method_07.
+*/
+void method_07()
+{
+    // definir dado
+    double soma = 0.0;
+    // identificar
+    IO_id("Method_07 - v0.0");
+    // chamar e receber resultado da funcao
+    soma = somarFracao1(5);
+    // mostrar resultado
+    IO_printf("soma de fracoes = %lf\n", soma);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_07 ( )
+
+/**
+ somarFracao2 - funcao para somar certa quantidade de fracoes.
+ @return soma dos valores
+ @param x - quantidade de valores a serem mostrados
+*/
+double somarFracao2(int x)
+{
+    // definir dados locais
+    double soma = 1.0;
+    double numerador = 0.0;
+    double denominador = 0.0;
+    int y = 0; // controle
+               // mostrar primeiro valor
+    IO_printf("%d: %7.4lf/%7.4lf\n", 1, 1.0, soma);
+    // repetir enquanto controle menor que a quantidade desejada
+    for (y = 1; y <= (x - 1); y = y + 1)
+    {
+        // calcular numerador
+        numerador = 2.0 * y - 1;
+        // calcular denominador
+        denominador = 2.0 * y;
+        // mostrar valor
+        IO_printf("%d: %7.4lf/%7.4lf = %lf\n",
+                  y + 1, numerador, denominador, (numerador / denominador));
+        // somar valor
+        soma = soma + numerador / denominador;
+    } // end for
+      // retornar resultado
+    return (soma);
+} // end somarFracao2 ( )
+
+/**
+ Method_08.
+*/
+void method_08()
+{
+    // definir dado
+    double soma = 0.0;
+    // identificar
+    IO_id("Method_08 - v0.0");
+    // chamar e receber resultado da funcao
+    soma = somarFracao2(5);
+    // mostrar resultado
+    IO_printf("soma de fracoes = %lf\n", soma);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_08 ( )
+
+/**
+ somarFracao3 - funcao para somar certa quantidade de fracoes.
+ @return soma dos valores
+ @param x - quantidade de valores a serem mostrados
+*/
+double somarFracao3(int x)
+{
+    // definir dados locais
+    double soma = 1.0;
+    int y = 0; // controle
+               // mostrar primeiro valor
+    IO_printf("%d: %7.4lf/%7.4lf\n", 1, 1.0, soma);
+    // repetir enquanto controle menor que a quantidade desejada
+    for (y = 1; y < x; y = y + 1)
+    {
+        // mostrar valor
+        IO_printf("%d: %7.4lf/%7.4lf = %7.4lf\n",
+                  y + 1, (2.0 * y), (2.0 * y + 1), ((2.0 * y) / (2.0 * y + 1)));
+        // somar valor
+        soma = soma + (2.0 * y) / (2.0 * y + 1);
+    } // end for
+      // retornar resultado
+    return (soma);
+} // end somarFracao3 ( )
+
+/**
+ Method_09.
+*/
+void method_09()
+{
+    // definir dado
+    double soma = 0.0;
+    // identificar
+    IO_id("EXEMPLO0509 - Method_09 - v0.0");
+    // chamar e receber resultado da funcao
+    soma = somarFracao3(5);
+    // mostrar resultado
+    IO_printf("soma de fracoes = %lf\n", soma);
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_09 ( )
+
+/**
+ multiplicarValores - funcao para multiplicar certa quantidade de valores impares.
+ @return produto de valores
+ @param x - quantidade de valores
+*/
+int multiplicarValores(int x)
+{
+    // definir dados locais
+    int produto = 1;
+    int y = 0; // controle
+               // repetir enquanto controle menor que a quantidade desejada
+    for (y = 1; y <= x; y = y + 1)
+    {
+        // mostrar valor
+        IO_printf("%d: %d\n", y, (2 * y - 1));
+        // somar valor
+        produto = produto * (2 * y - 1);
+    } // end for
+      // retornar resultado
+    return (produto);
+} // end multiplicarValores ( )
+/**
+ Method_10.
+*/
+void method_10()
+{
+    // identificar
+    IO_id("Method_10 - v0.0");
+    // mostrar produto de valores
+    IO_printf("%s%d\n", "produto = ", multiplicarValores(5));
+    // encerrar
+    IO_pause("Apertar ENTER para continuar");
+} // end method_10 ( )
+
+int main()
+{
+    // definir dado
+    int x = 0;
+    // repetir até desejar parar
+    do
+    {
+        // identificar
+        IO_id("Exemplo0400 - Programa - v0.0");
+        // ler do teclado
+        IO_println("Opcoes");
+        IO_println(" 0 - parar");
+        IO_println(" 1 - Method01");
+        IO_println(" 2 - Method02");
+        IO_println(" 3 - Method03");
+        IO_println(" 4 - Method04");
+        IO_println(" 5 - Method05");
+        IO_println(" 6 - Method06");
+        IO_println(" 7 - Method07");
+        IO_println(" 8 - Method08");
+        IO_println(" 9 - Method09");
+        IO_println("10 - Method10");
+        IO_println("");
+        x = IO_readint("Entrar com uma opcao: ");
+        // testar valor
+        switch (x)
+        {
+        case 0:
+            break;
+        case 1:
+            method_01();
+            break;
+        case 2:
+            method_02();
+            break;
+        case 3:
+            method_03();
+            break;
+        case 4:
+            method_04();
+            break;
+        case 5:
+            method_05();
+            break;
+        case 6:
+            method_06();
+            break;
+        case 7:
+            method_07();
+            break;
+        case 8:
+            method_08();
+            break;
+        case 9:
+            method_09();
+            break;
+        case 10:
+            method_10();
+            break;
+        default:
+            IO_pause(IO_concat("Valor diferente das opcoes [0,1] (",
+                               IO_concat(IO_toString_d(x), ")")));
+        } // end switch
+    } while (x != 0);
+
+    // encerrar
+    IO_pause("Apertar ENTER para terminar");
+    return (0);
+} // end main ( )
+
+/*
+---------------------------------------------- documentacao complementar
+---------------------------------------------- notas / observacoes / comentarios
+---------------------------------------------- previsao de testes
+---------------------------------------------- historico
+Versao Data Modificacao
+0.1 __/__ esboco
+---------------------------------------------- testes
+Versao Teste
+0.1 01. ( OK ) identificacao de programa
+*/
---
+++ EDs/Ed05/Exercicios05.c
@@ -0,0 +1,312 @@
+/*
+ * -------------------------- Documentacao
+ * Pontificia Universidade Catolica de Minas Gerais
+ * Curso de Ciencia da Computacao
+ * Algoritmos e Estruturas de Dados I
+ *
+ * Exercicios0X - v0.0. - __ / __ / ____
+ * Author: Vinicius Miranda de Araujo
+ *
+ * Para compilar em terminal (janela de comandos):
+ *      Linux : gcc -o Exercicios0X Exercicios0X.c
+ *      Windows: gcc -o Exercicios0X Exercicios0X.c
+ *
+ * Para executar em terminal (janela de comandos):
+ *      Linux : ./Exercicios0X
+ *      Windows: Exercicios0X
+ *
+ */
+
+// -------------------------- lista de dependencias
+
+#include "libshow.h"
+
+// -------------------------- tratamento de erro
+
+// -------------------------- definicao de constantes
+
+// -------------------------- definicao de variaveis globais
+
+// -------------------------- definicao de prototipos
+
+void menuOpcoes();
+void exercicio0X11();
+void exercicio0X12();
+void exercicio0X13();
+void exercicio0X14();
+void exercicio0X15();
+void exercicio0X16();
+void exercicio0X17();
+void exercicio0X18();
+void exercicio0X19();
+void exercicio0X20();
+void exercicio0XE1();
+void exercicio0XE2();
+
+// -------------------------- definicao do metodo principal
+
+int main(void)
+{
+    int opcao = 0;
+
+    do
+    {
+        clear();
+
+        // mostrar identificacao do autor e programa
+        id("Programa: Exercicios0X - v0.0");
+
+        // mostrar menu de opcoes
+        menuOpcoes();
+
+        // ler opcao do teclado
+        opcao = readint("Opcao = ");
+
+        // executar a opcao escolhida
+        switch (opcao)
+        {
+        case 0:
+            printf("\n%s\n", "Programa Encerrado...");
+            break;
+        case 1:  exercicio0X11(); break;
+        case 2:  exercicio0X12(); break;
+        case 3:  exercicio0X13(); break;
+        case 4:  exercicio0X14(); break;
+        case 5:  exercicio0X15(); break;
+        case 6:  exercicio0X16(); break;
+        case 7:  exercicio0X17(); break;
+        case 8:  exercicio0X18(); break;
+        case 9:  exercicio0X19(); break;
+        case 10: exercicio0X20(); break;
+        case 11: exercicio0XE1(); break;
+        case 12: exercicio0XE2(); break;
+        default:
+            printf("\n%s\n", "ERRO: opcao invalida");
+            break;
+        } // fim switch
+
+    } while (opcao != 0);
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para terminar!");
+    getchar();
+
+    return (0);
+}
+
+// -------------------------- definicao de metodos
+
+/**
+ * Menu de opcoes
+ */
+void menuOpcoes()
+{
+    printf("\n");
+    printf("%s\n\n", "Escolha alguma das opcoes a seguir:");
+    printf("%s\n", "  0 - Encerrar programa");
+    printf("%s\n", "  1 - Exercicio 0X11");
+    printf("%s\n", "  2 - Exercicio 0X12");
+    printf("%s\n", "  3 - Exercicio 0X13");
+    printf("%s\n", "  4 - Exercicio 0X14");
+    printf("%s\n", "  5 - Exercicio 0X15");
+    printf("%s\n", "  6 - Exercicio 0X16");
+    printf("%s\n", "  7 - Exercicio 0X17");
+    printf("%s\n", "  8 - Exercicio 0X18");
+    printf("%s\n", "  9 - Exercicio 0X19");
+    printf("%s\n", " 10 - Exercicio 0X20");
+    printf("%s\n", " 11 - Exercicio 0XE1");
+    printf("%s\n", " 12 - Exercicio 0XE2");
+    printf("\n");
+} // fim menuOpcoes()
+
+/**
+ * Metodo01.
+ */
+void exercicio0X11()
+{
+    // identificacao
+    id("Exercicio 0X11:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X11
+
+/**
+ * Metodo02.
+ */
+void exercicio0X12()
+{
+    // identificacao
+    id("Exercicio 0X12:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X12
+
+/**
+ * Metodo03.
+ */
+void exercicio0X13()
+{
+    // identificacao
+    id("Exercicio 0X13:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X13
+
+/**
+ * Metodo04.
+ */
+void exercicio0X14()
+{
+    // identificacao
+    id("Exercicio 0X14:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X14
+
+/**
+ * Metodo05.
+ */
+void exercicio0X15()
+{
+    // identificacao
+    id("Exercicio 0X15:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X15
+
+/**
+ * Metodo06.
+ */
+void exercicio0X16()
+{
+    // identificacao
+    id("Exercicio 0X16:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X16
+
+/**
+ * Metodo07.
+ */
+void exercicio0X17()
+{
+    // identificacao
+    id("Exercicio 0X17:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X17
+
+/**
+ * Metodo08.
+ */
+void exercicio0X18()
+{
+    // identificacao
+    id("Exercicio 0X18:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X18
+
+/**
+ * Metodo09.
+ */
+void exercicio0X19()
+{
+    // identificacao
+    id("Exercicio 0X19:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X19
+
+/**
+ * Metodo10.
+ */
+void exercicio0X20()
+{
+    // identificacao
+    id("Exercicio 0X20:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0X20
+
+/**
+ * Metodo11.
+ */
+void exercicio0XE1()
+{
+    // identificacao
+    id("Exercicio 0XE1:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0XE1
+
+/**
+ * Metodo12.
+ */
+void exercicio0XE2()
+{
+    // identificacao
+    id("Exercicio 0XE2:");
+
+    // programa
+
+    // encerrar
+    printf("\n%s\n", "Aperte ENTER para continuar!");
+    getchar();
+    clear();
+} // fim exercicio0XE2
