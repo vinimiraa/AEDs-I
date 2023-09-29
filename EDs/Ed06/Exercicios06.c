90d69eb46a2352d5efe0ad0445e625d0bc92ed09
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2023-09-29 20:42:36 -0300

    Update EDs

--- EDs/Ed06/Exercicios06.c
+++ EDs/Ed06/Exercicios06.c
@@ -132,10 +132,7 @@ void metodo0611(int x,int n)
     if(n >= 1) // teste / base
     {
         print("%d ", x);
-        if(x % 2 != 0)
-            metodo0611( x + 1, n - 1 ); // motor
-        else
-            metodo0611( x + 2, n - 1 ); // motor
+        metodo0611(x+7, n-1);
     }
 }
 /**
@@ -150,7 +147,7 @@ void exercicio0611()
     int n = 0;
     n = readint("Digite um valor: ");
 
-    print("\n%s\n", "Valores pares em ordem crescente comecando no valor 7:");
+    print("\n%s\n", "Valores multiplos de 7, em ordem crescente, comecando no valor 7:");
     metodo0611(7,n);
     print("\n");
 
@@ -180,7 +177,7 @@ void exercicio0612()
     int n = 0;
     n = readint("Digite um valor: ");
 
-    print("\n%s\n", "Multiplos de 7 em ordem decrescente encerrando no valor 7:");
+    print("\n%s\n", "Multiplos de 7, em ordem decrescente, encerrando no valor 7:");
     metodo0612(7,n);
     print("\n");
 
@@ -194,7 +191,7 @@ void metodo0613(int x, int n)
 {
     if(n >= 1)
     {
-        print("%d ", x);
+        print("1/%d ", x);
         if(x == 1)
             metodo0613( x + 6, n - 1 );
         else
@@ -213,7 +210,7 @@ void exercicio0613()
     int n = 0;
     n = readint("Digite um valor: ");
 
-    print("\n%s", "Sequencia = ");
+    print("\n%s\n", "Valores da sequencia de inversos dos multiplos de 7, comecando em 1: ");
     metodo0613(1,n);
     print("\n");
 
@@ -236,7 +233,7 @@ void metodo0614(int x, int n)
             metodo0614(x + 1, n);
         if(x == 1)
         {
-            print("1");
+            print("1/1");
         }
     }
 }
@@ -252,7 +249,7 @@ void exercicio0614()
     int n = 0;
     n = readint("Digite um valor: ");
 
-    print("\n%s", "Sequencia = ");
+    print("\n%s\n", "Valores da sequencia de inversos dos multiplos de 7, terminando em 1: ");
     metodo0614(1,n);
     print("\n");
 
@@ -262,13 +259,13 @@ void exercicio0614()
     clear();
 } // fim exercicio0614
 
-int metodo0615(int x, int n)
+int metodo0615(int x, int y, int n)
 {
     int soma = 0;
     if(n >= 1)
     {
         // print("\nAntes  : soma = %2d   x = %2d   n = %2d", soma, x, n);
-        soma = x + metodo0615( x + 2, n - 1 );
+        soma = x + metodo0615( x + y, y + 2, n - 1 );
         // print("\nDepois : soma = %2d   x = %2d   n = %2d", soma, x, n);
     }
     return ( soma );
@@ -285,8 +282,8 @@ void exercicio0615()
     int n = 0;
     n = readint("Digite uma quantidade: ");
 
-    print("\n%s%d\n", "Soma dos primeiros valores impares positivos comecando no valor 7 = ", 
-                        metodo0615(7,n) );
+    print("\n%s%d\n", "Soma de valores gerados pela adicao dos primeiros valores impares positivos ao valor 7 = ", 
+                        metodo0615(7,1,n) );
 
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
@@ -294,18 +291,19 @@ void exercicio0615()
     clear();
 } // fim exercicio0615
 
-double metodo0616(int x, int n)
+double metodo0616(double nume, double deno, int n)
 {
     double soma = 0.0;
-    if(n >= 1)
-    {
-        // print("\nAntes  : soma = %lf   x = %2d   n = %2d", soma, x, n);
-        double inverso = 1.0/x;
-        if(x % 2 != 0)
-            soma = inverso + metodo0616( x + 1, n - 1);
+    if(n > 0)
+    {   
+        // print("\nAntes  : nume = %.lf   deno = %.lf   n = %2d   soma = %lf",nume,deno,n,soma);
+        if( (int)deno % 2 == 0 || deno == 7 )
+        {
+            soma = nume/deno + metodo0616(nume, deno + 7, n - 1); 
+            // print("\nDentro : nume = %.lf   deno = %.lf   n = %2d   soma = %lf",nume,deno,n,soma);
+        }
         else
-            soma = inverso + metodo0616( x + 2, n - 1 );
-        // print("\nDepois : soma = %lf   x = %2d   n = %2d", soma, x, n);
+            soma = 0 + metodo0616(nume, deno + 7, n);
     }
     return ( soma );
 }
@@ -321,14 +319,26 @@ void exercicio0616()
     int n = 0;
     n = readint("Digite uma quantidade: ");
 
-    print("\n%s%lf\n", "Soma dos inversos (1/x) dos primeiros valores pares positivos comecando no valor 7 = ", 
-                        metodo0616(7,n) );
+    print("\n%s%lf\n", "Soma dos inversos (1/x) dos primeiros valores pares positivos multiplos do valor 7 = ", 
+                        metodo0616(1.0,7.0,n) );
+                        
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
     getchar();
     clear();
 } // fim exercicio0616
 
+void metodo0617(char* palavra, int tamanho)
+{   
+    if( palavra && tamanho >= 0 )
+    {
+        metodo0617(palavra,tamanho-1);
+        if( isSpace(palavra[tamanho]) )
+            print("\n0");
+        else 
+            print("\n%c", palavra[tamanho]);
+    }
+}
 /**
  * Metodo07.
  */
@@ -338,13 +348,38 @@ void exercicio0617()
     id("Exercicio 0617:");
 
     // programa
+    char *palavra = 0;
+    int tamanho = 0;
+    palavra = readstring("Digite uma cadeia de caracteres: ");
 
+    tamanho = strlen(palavra);
+
+    metodo0617(palavra, tamanho);
+
+    free(palavra);
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
     getchar();
     clear();
 } // fim exercicio0617
 
+int metodo0618(char *palavra, int tamanho)
+{
+    int count = 0;
+    if( palavra && tamanho >= 0 )
+    {
+        count = metodo0618(palavra, tamanho - 1); 
+        if( isDigit(palavra[tamanho]) )
+        {
+            if( palavra[tamanho] % 2 == 0)
+            {
+                // print( "\n%c", palavra[tamanho] );
+                count = count + 1;
+            }
+        }
+    }
+    return (count);
+}
 /**
  * Metodo08.
  */
@@ -354,13 +389,38 @@ void exercicio0618()
     id("Exercicio 0618:");
 
     // programa
+    char *palavra = 0;
+    int tamanho = 0;
+    int resultado = 0;
+    palavra = readstring("Digte uma cadeia de caracteres: ");
 
+    tamanho = strlen(palavra);
+
+    resultado = metodo0618(palavra,tamanho);
+
+    print("\n%s%d\n", "A quantidade de digitos pares que a cadeia de caracteres possui e = ",
+                        resultado);
+
+    free(palavra);
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
     getchar();
     clear();
 } // fim exercicio0618
 
+int metodo0619(char *palavra, int tamanho)
+{
+    int count = 0;
+    if(palavra && tamanho >= 0)
+    {
+        count = metodo0619(palavra, tamanho - 1);
+        if(isUpper(palavra[tamanho]) && palavra[tamanho] > 'K')
+        {
+            count = count + 1;
+        }
+    }
+    return (count);
+}
 /**
  * Metodo09.
  */
@@ -370,6 +430,16 @@ void exercicio0619()
     id("Exercicio 0619:");
 
     // programa
+    char *palavra = 0;
+    int tamanho = 0;
+    int resultado = 0;
+    palavra = readstring("Digite uma cadeia de caracteres: ");
+
+    tamanho = strlen(palavra);
+
+    resultado = metodo0619(palavra,tamanho);
+
+    print("\n%s%d\n", "A quantidade de maiusculas maiores que 'K' na cadeia de caracteres = ", resultado);
 
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
@@ -377,8 +447,77 @@ void exercicio0619()
     clear();
 } // fim exercicio0619
 
+int fibonacci ( int n )
+{
+    int resposta = 0;
+    if ( n == 1 || n == 2 )
+        {
+        resposta = 1;
+        }
+    else
+    {
+        if ( n > 2 )
+        {
+            resposta = fibonacci (n - 1) + fibonacci (n - 2);
+        } 
+    } 
+    return ( resposta );
+} 
+int metodo0620(int x, int y, int n)
+{
+    int soma = 0;
+    int z = 0; 
+    if( x <= n )
+    {
+        print("\n1 : x = %2d   y = %2d   z = %2d   s = %2d", x,y,z,soma);
+        z =fibonacci(y);
+        if( z % 2 == 0)  
+        {   
+            soma = soma + z;
+            // print("\n2 : x = %2d   y = %2d   z = %2d   s = %2d", x,y,z,soma);
+            metodo0620(x + 1, y + 1, n);
+            // print("\n2a: x = %2d   y = %2d   z = %2d   s = %2d", x,y,z,soma);
+        }
+        else
+        {
+            // print("\n3 : x = %2d   y = %2d   z = %2d   s = %2d", x,y,z,soma);
+            metodo0620(x, y + 1, n); 
+            // print("\n3a: x = %2d   y = %2d   z = %2d   s = %2d", x,y,z,soma);
+        }
+        print("\n1a: x = %2d   y = %2d   z = %2d   s = %2d", x,y,z,soma);
+    }
+    return (soma);
+
+    // int z = 0;
+    // int y = 1;
+    // int x = 1;
+    // int soma = 0;
+
+    /*Solucao usando for*/
+    // for(y = 1;x <= n; y = y + 1)
+    // {
+    //     z = fibonacci(y);
+    //     if( z % 2 == 0)
+    //     {
+    //         soma = soma + z;
+    //         x = x + 1;
+    //     }
+    // }
+
+    /*Solucao feita em Sala pelo Professor Theldo*/
+    // while (x <= n)
+    // {
+    //     z = fibonacci(y);
+    //     if( z % 2 == 0)
+    //     {
+    //         soma = soma + z;
+    //         x = x + 1;
+    //     }
+    //     y = y + 1;
+    // }
+}
 /**
- * Metodo10.
+ * Metodo10.    
  */
 void exercicio0620()
 {
@@ -386,6 +525,13 @@ void exercicio0620()
     id("Exercicio 0620:");
 
     // programa
+    int n = 0;
+    int resultado = 0;
+    n = readint("Digite uma quantidade: ");
+
+    resultado = metodo0620(1,1,n);
+    
+    print("\n%s%d\n", "Soma da quantidade em numeros pares da serie de Fibonacci = ", resultado);
 
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
@@ -393,6 +539,23 @@ void exercicio0620()
     clear();
 } // fim exercicio0620
 
+int somarE1(int x,int n,int p)
+{
+    int soma = 1;
+    if(n > 1)
+    {
+        // print("\nA: %d: x = %2d p = %2d s = %2d", n, x, p, soma);
+        soma = pow(x,p) + somarE1( x, n - 1, p+2);
+        // print("\nD: %d: x = %2d p = %2d s = %2d", n, x, p, soma);
+    }
+    return (soma);
+}
+int metodo06E1(int x, int n)
+{
+    int resultado = 0;
+    resultado = somarE1(x,n,2);
+    return (resultado);
+}
 /**
  * Metodo11.
  */
@@ -402,6 +565,15 @@ void exercicio06E1()
     id("Exercicio 06E1:");
 
     // programa
+    int x = 0;
+    int n = 0;
+    int resultado = 0;
+    x = readint("Digite um valor: ");
+    n = readint("Digite uma quantidade: ");
+
+    resultado = metodo06E1(x,n);
+
+    print("\n%s%d\n", "Soma = ", resultado);
 
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
@@ -409,6 +581,39 @@ void exercicio06E1()
     clear();
 } // fim exercicio06E1
 
+double somarE2(int n, double soma, double nume, double deno)
+{
+    if(n > 0)
+    {
+        if(nume == 1 && deno == 2)
+        {
+            // print("\n%d: %lf/%lf", n, nume, deno);
+            soma = somarE2(n-1,
+                            soma + (nume/factorial(deno)),
+                            nume + 1,
+                            deno + 1);
+        }
+        else
+        {
+            // print("\n%d: %lf/%lf", n, nume, deno);
+            soma = somarE2(n-1,
+                            soma + (nume/factorial(deno)),
+                            nume + 2,
+                            deno + 2);
+        }
+    }
+    return (soma);
+}
+double metodo06E2(int n)
+{
+    double resultado = 0.0;
+    if( n > 0 )
+    {
+        // print("%d: %lf", n, 1.0);
+        resultado = somarE2(n-1,1.0,1.0,2.0);
+    }
+    return (resultado);
+}
 /**
  * Metodo12.
  */
@@ -418,7 +623,13 @@ void exercicio06E2()
     id("Exercicio 06E2:");
 
     // programa
+    int n = 0;
+    double resultado = 0;
+    n = readint("Digite a quantidade de termos: ");
 
+    resultado = metodo06E2(n);
+
+    print("\n%s%lf\n", "Soma = ", resultado);
     // encerrar
     printf("\n%s\n", "Aperte ENTER para continuar!");
     getchar();
