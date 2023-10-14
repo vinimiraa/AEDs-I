fbc66987bd3dbb819ae35be9524d9c3a7df6d8b6
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2023-10-14 00:05:54 -0300

    Update EDs

--- EDs/Ed08/Exercicios08.c
+++ EDs/Ed08/Exercicios08.c
@@ -154,7 +154,7 @@ void readintArray(int n, int array[])
         strcpy(text, "");
         do{
             y = readint( concat( concat(text, inttostr(x)), " : " ) );
-        }while ( (y <= 0) || (y%2 == 0) );
+        }while ( isNegative(y) || isEven(y) );
         array[x] = y;
     }
 } // end readIntArray ( )
@@ -169,6 +169,7 @@ void exercicio0811()
     // programa
     int n = 0;
     n = readint("Digite um tamanho: "); print("\n");
+    int array[n];
 
     if( n <= 0 )
     {
@@ -176,7 +177,6 @@ void exercicio0811()
     }
     else
     {
-        int array[n];
         readintArray(n,array);
         printArray(n,array);
     }
@@ -186,6 +186,45 @@ void exercicio0811()
     clear();
 } // fim exercicio0811
 
+int freadArraySize(string fileName)
+{
+    int n = 0;
+    FILE *arquivo = fopen(fileName, "rt");
+
+    fscan(arquivo, "%d", &n);
+    if (n <= 0)
+    {
+        print("\n%s\n","ERRO: Valor invalido.");
+        n = 0;
+    }
+    fclose(arquivo);
+
+    return (n);
+} // end freadArraySize ( )
+void freadIntArray(string fileName, int n, int array[])
+{
+    int x = 0;
+    int y = 0;
+    FILE *arquivo = fopen(fileName, "rt");
+
+    fscan(arquivo, "%d", &x);
+    if (n <= 0 || n > x)
+    {
+        print("\n%s\n","ERRO: Tamanho invalido.");
+    }
+    else
+    {
+        x = 0;
+        while (!feof(arquivo) && x < n)
+        {
+            fscan(arquivo, "%d", &y);
+            array[x] = y;
+            x = x + 1;
+        }
+    }
+
+    fclose(arquivo);
+} // end freadIntArray ( )
 /**
  * Metodo02.
  */
@@ -195,12 +234,58 @@ void exercicio0812()
     id("Exercicio 0812:");
 
     // programa
+    int n     = 0;
+    int count = 0;
+    int x     = 0;
+    FILE *arquivo = fopen("DADOS02.TXT", "wt");
+    
+    //ler tamanho do array
+    n = freadArraySize("ARRAY02.TXT");
+    if( n <= 0 )
+    {
+        print("\n%s\n", "ERRO: Tamanho Invalido.");
+    }
+    else
+    {
+        int array[n];
+        //ler os numeros do array
+        freadIntArray("ARRAY02.TXT", n, array);
+        print("\n%s\n", "Dados lidos do arquivo ARRAY02.TXT.");
+        // saber quantos satisfazem a condicao
+        for(int i = 0; i < n; i++)
+        {
+            if( isPositive(array[i]) && isOdd(array[i]) )
+                count++;
+        }
+        //mostrar quantos satisfazem a condicao
+        fprint(arquivo, "%d\n", count);
+        //mostrar os numeros q satisfazem a condicao
+        for(int i = 0; i < n; i++)
+        {
+            x = array[i];
+            if( isPositive(x) && isOdd(x) )
+            {
+                fprint(arquivo,"%d\n", x);
+            }
+        }
+        print("\n%s\n", "Dados gravados no arquivo DADOS02.TXT.");
+    }
+    fclose(arquivo);
 
     // encerrar
     pause("Aperte ENTER para continuar!");
     clear();
 } // fim exercicio0812
 
+int randnum(int a, int b)
+{
+    int x = 0;
+    while( x < a || b < x )
+    {
+        x = rand() % 100;
+    }
+    return(x);
+}
 /**
  * Metodo03.
  */
@@ -210,12 +295,65 @@ void exercicio0813()
     id("Exercicio 0813:");
 
     // programa
+    int a = 0; //limite inicial
+    int b = 0; //limite final 
+    int n = 0; //quantidade de elementos
+    int x = 0; //valor aleatorio
+    int i = 0; //variavel para utilizar no arranjo
+    FILE *arquivo = fopen("DADOS03.TXT", "wt");
+    srand( time(NULL) );
+    
+    a = readint("Digite o limite inicial do intervalo: "          );
+    b = readint("Digite o limite final do intervalo  : "          );
+    n = readint("Digite a quantidades de elementos no intervalo: ");
 
+    if( a > b )
+    {
+        print("\n%s\n", "ERRO: Limite Inicial Maior que o Limite Final");
+    }
+    else
+    {
+        int array[n];
+        fprint(arquivo, "%d\n", n);
+        for( i = 0; i < n; i++ )
+        {
+            x = randnum(a,b);
+            array[i] = x;
+            fprint(arquivo,"%d\n", array[i] );
+        }
+        print("\n%s\n", "Dados gravados no arquivo DADOS03.TXT.");
+        // while(n > i)
+        // {
+        //     x = rand() % 120;
+        //     if(inClosedInterval(x,a,b))
+        //     {
+        //         array[i] = x;
+        //         fprint(arquivo, "%d\n", array[i]);
+        //         i++;
+        //     }
+        // }
+    }
+
+    fclose(arquivo);
     // encerrar
     pause("Aperte ENTER para continuar!");
     clear();
 } // fim exercicio0813
 
+int minoddArray(int n, int array[])
+{
+    int menor = 0;
+    int i     = 0;
+    menor = array[0];
+    for( i  = 0; i < n; i = i + 1)
+    {
+        if( (array[i] < menor) && (array[i] % 2 != 0) )
+        {
+            menor = array[i];
+        }
+    }
+    return (menor);
+}
 /**
  * Metodo04.
  */
@@ -225,12 +363,37 @@ void exercicio0814()
     id("Exercicio 0814:");
 
     // programa
+    int n = 0;
+    int x = 0;
 
+    n = freadArraySize("DADOS03.TXT");
+
+    int array[n];
+
+    freadIntArray("DADOS03.TXT", n, array);
+
+    x = minoddArray(n,array);
+    print("%s%d\n", "O menor valor impar do arquivo DADOS03.TXT = ", x);
+    
     // encerrar
     pause("Aperte ENTER para continuar!");
     clear();
 } // fim exercicio0814
 
+int maxevenArray(int n, int array[])
+{
+    int maior = 0;
+    int i     = 0;
+    maior = array[0];
+    for( i = 0; i < n; i = i + 1 )
+    {
+        if( (array[i] > maior) && (array[i] % 2 == 0) )
+        {
+            maior = array[i];
+        }
+    }
+    return (maior);
+}
 /**
  * Metodo05.
  */
@@ -240,12 +403,34 @@ void exercicio0815()
     id("Exercicio 0815:");
 
     // programa
+    int n = 0;
+    int x = 0;
+
+    n = freadArraySize("DADOS03.TXT");
+
+    int array[n];
+
+    freadIntArray("DADOS03.TXT", n, array);
+
+    x = maxevenArray(n,array);
+    print("%s%d\n", "O maior valor par do arquivo DADOS03.TXT = ", x);
 
     // encerrar
     pause("Aperte ENTER para continuar!");
     clear();
 } // fim exercicio0815
 
+double findmedia(int n, int array[])
+{
+    double media = 0.0;
+    double soma = 0.0;
+    for(int i = 0; i < n; i = i + 1)
+    {
+        soma = soma + array[i];
+    }
+    media = soma / n;
+    return ( media );
+}
 /**
  * Metodo06.
  */
@@ -255,12 +440,75 @@ void exercicio0816()
     id("Exercicio 0816:");
 
     // programa
+    FILE *arquivo1 = fopen("MEDIAMENOR.TXT", "wt");
+    FILE *arquivo2 = fopen("MEDIAMAIOR.TXT", "wt");
+
+    int n = 0, count1 = 0, count2 = 0;
+    double media = 0.0;
+
+    n = freadArraySize("DADOS03.TXT");
+
+    if( n <= 0 )
+    {
+        print("\n%s\n", "ERRO: Tamanho invalido.");
+    }
+    else
+    {
+        int array[n];
+
+        freadIntArray("DADOS03.TXT", n, array);
+
+        media = findmedia(n,array);
+        // print("%.5lf", media);
+        //contar quantos dados sao maiores ou iguais e menores que a media
+        for(int i = 0; i < n; i = i + 1)
+        {
+            if(array[i] < media)
+            {
+                count1++;
+            } 
+            else
+            if( array[i] >= media)
+            {
+                count2++;
+            }
+        }
+        fprint(arquivo1, "%d\n", count1); // quant. maior ou igual a media
+        fprint(arquivo2, "%d\n", count2); // quant. menor que a media
+        for(int i = 0; i < n; i = i + 1)
+        {
+            if(array[i] < media)
+            {
+                fprint(arquivo1, "%d\n", array[i]);
+            } 
+            else
+            if( array[i] >= media)
+            {
+                fprint(arquivo2, "%d\n", array[i]);
+            }
+        }
+        print("\n%s\n", "Os dados do arquivo DADOS03.TXT que sao menores que a media foram gravados no arquivo MEDIAMENOR.TXT");
+        print("\n%s\n", "Os dados do arquivo DADOS03.TXT que sao maiores que a media foram gravados no arquivo MEDIAMAIOR.TXT");
+    }
+
+    fclose(arquivo1);
+    fclose(arquivo2);
 
     // encerrar
     pause("Aperte ENTER para continuar!");
     clear();
 } // fim exercicio0816
 
+bool isDecrescente(int n,int array[])
+{
+    bool resultado = true;
+    for(int i = 0; i < n - 1; i++)
+    {
+        if( array[i] < array[i + 1] )
+            resultado = false;
+    }
+    return ( resultado );
+}
 /**
  * Metodo07.
  */
@@ -270,6 +518,30 @@ void exercicio0817()
     id("Exercicio 0817:");
 
     // programa
+    int array1[10]; //= {10,9,8,7,6,5,4,3,2,1};
+    int array2[] = {10,9,8,7,6,5,4,3,2,1};
+
+    srand( time(NULL) );
+    for( int i = 0; i < 10; i = i + 1)
+    {
+        array1[i] = rand() % 110;
+    }
+
+    print("%s\n", "Arranjo 1: ");
+    printArray(10, array1);
+
+    print("%s\n", "Arranjo 2: ");
+    printArray(10, array2);
+
+    if( isDecrescente(10,array1) )
+        printf("\n%s\n", "O arranjo 1 esta ordenado de forma decrescente."    );
+    else
+        printf("\n%s\n", "O arranjo 1 nao esta ordenado de forma decrescente.");
+    
+    if( isDecrescente(10,array2) )
+        printf("\n%s\n", "O arranjo 2 esta ordenado de forma decrescente."    );
+    else
+        printf("\n%s\n", "O arranjo 2 nao esta ordenado de forma decrescente.");
 
     // encerrar
     pause("Aperte ENTER para continuar!");
