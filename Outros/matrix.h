d893f192a6e891f39866d9dafd2c6d39524149f3
Vinícius Miranda de Araújo <vinicius.araujo.1457564@sga.pucminas.br>
2023-10-25 21:32:33 -0300

    Update Outros

---
+++ Outros/matrix.h
@@ -0,0 +1,38 @@
+/**
+    Biblioteca Matrix - IO - Console Input/Output library.
+    PUC-Minas - ICEI
+    @author  Vinícius Miranda de Araújo
+    @version 0.1 - 2023-2b
+
+    MATRIX - v2023-2_0.1 - 25 / 10 / 2023
+    Author: Vinícius Miranda de Araújo
+
+*/
+
+// ---------------------- lista de dependencias
+
+#include <stdio.h>   // para entradas e saidas
+#include <stdlib.h>  // para a biblioteca padrao
+#include <stddef.h>  // para definicoes basicas
+#include <stdbool.h> // para definicoes logicas
+#include <stdarg.h>  // para tratar argumentos
+#include <string.h>  // para cadeias de caracteres
+#include <ctype.h>   // para tipos padroes
+#include <math.h>    // para definicoes matematicas
+#include <time.h>    // para medir tempo
+#include <wchar.h>   // para 16-bit characters
+
+// ---------------------- redefinicoes uteis
+
+#ifndef _MATRIX_H_
+#define _MATRIZ_H_
+
+// ---------------------- definicoes de constantes
+
+const int MAX_SIZE = 10 ;  // definir tamanho padrao para matrizes
+
+// ---------------------- definicoes de variaveis globais
+
+// ---------------------- para entradas e saidas
+
+#endif
